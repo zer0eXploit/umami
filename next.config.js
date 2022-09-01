5935b94d43c7b241ae318a9eb07b8719351fea65
Superior Ice <37069426+zer0eXploit@users.noreply.github.com>
2022-09-01 09:28:58 +0630

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -23,6 +23,22 @@ const headers = [
     key: 'Content-Security-Policy',
     value: contentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
   },
+  { 
+    key: "Access-Control-Allow-Credentials",
+    value: "true"
+  },
+  { 
+    key: "Access-Control-Allow-Origin",
+    value: "*"
+  },
+  { 
+    key: "Access-Control-Allow-Methods",
+    value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" 
+  },
+  { 
+    key: "Access-Control-Allow-Headers",
+    value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" 
+  },
 ];
 
 if (process.env.FORCE_SSL) {
